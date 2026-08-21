81e2e99e75aeb297a4795c8abd71997877076ea1
Malkrich <mathurin.lemarie@gmail.com>
2026-08-21 11:55:18 +0200

    Improve Application Core API

--- Soup/Src/Core/Core/Window.h
+++ Soup/Src/Core/Core/Window.h
@@ -8,68 +8,66 @@ struct GLFWwindow;
 namespace Soup
 {
 
+  struct WindowSpecifications
+  {
+    WindowSpecifications()
+      : Name("Window")
+      , Width(1280)
+      , Height(720)
+      , VSync(true)
+    {
+    }
+
+    WindowSpecifications(const std::string& nameParam, uint32_t widthParam, uint32_t heightParam, bool vsync = true)
+      : Name(nameParam)
+      , Width(widthParam)
+      , Height(heightParam)
+      , VSync(vsync)
+    {
+    }
+
+    std::string Name;
+
+    uint32_t Width;
+    uint32_t Height;
+
+    bool VSync = true;
+
+    EventDispatcher::EventCallbackFn CallbackFn;
+  };
+
   class Window
   {
   public:
-    struct WindowSettings
-    {
-    public:
-      WindowSettings()
-        : Name("Window")
-        , Width(1280)
-        , Height(720)
-        , VSync(true)
-      {
-      }
-
-      WindowSettings(const std::string& nameParam, uint32_t widthParam, uint32_t heightParam, bool vsync = true)
-        : Name(nameParam)
-        , Width(widthParam)
-        , Height(heightParam)
-        , VSync(vsync)
-      {
-      }
-
-    public:
-      std::string Name;
-
-      uint32_t Width;
-      uint32_t Height;
-
-      bool VSync = true;
-
-      EventDispatcher::EventCallbackFn CallbackFn;
-    };
-
   public:
-    Window(const WindowSettings& windowSettings);
+    Window(const WindowSpecifications& windowSettings);
 
     ~Window();
 
-    uint32_t GetWidth() const { return m_Settings.Width; }
-    uint32_t GetHeight() const { return m_Settings.Height; }
+    uint32_t GetWidth() const { return m_Specs.Width; }
+    uint32_t GetHeight() const { return m_Specs.Height; }
 
     void SetEventCallbackFunction(const EventDispatcher::EventCallbackFn& callbackFn)
     {
-      m_Settings.CallbackFn = callbackFn;
+      m_Specs.CallbackFn = callbackFn;
     }
 
     void* GetWindowHandle() const { return (void*)m_Window; }
 
-    bool IsVSync() const { return m_Settings.VSync; }
+    bool IsVSync() const { return m_Specs.VSync; }
     void SetVSync(bool enable);
 
     void OnUpdate();
 
   private:
-    void Initialize(const WindowSettings& windowSettings);
+    void Initialize(const WindowSpecifications& windowSettings);
 
   private:
     GLFWwindow* m_Window;
 
     Scope<GraphicsContext> m_Context;
 
-    WindowSettings m_Settings;
+    WindowSpecifications m_Specs;
   };
 
 }
